e0cc6496b5a6b3facaba0ee1659b81f8a32d6be8
tarachom <tarachom@i.ua>
2022-06-03 14:30:04 +0300

    Add project files.

---
+++ WindowsFormsXSLT/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+
+using System.Xml.Xsl;
+using System.IO;
+
+namespace WindowsFormsXSLT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Шлях до папки (bin\Debug)
+            string pathToApp = Path.GetDirectoryName(Application.ExecutablePath);
+
+            //Шлях до шаблону XSLT
+            string pathToTemplate = Path.Combine(pathToApp, "..\\..\\Template.xslt");
+
+            //Шлях до XML даних
+            string pathToXmlData = Path.Combine(pathToApp, "..\\..\\XmlData.xml");
+
+            //Шлях до файлу куди буде записаний результат трансформації XML даних
+            string pathToSaveResult = Path.Combine(pathToApp, "..\\..\\Result.html");
+
+
+            XslCompiledTransform xsltTransform = new XslCompiledTransform();
+
+            //Завантаження шаблону
+            xsltTransform.Load(pathToTemplate, new XsltSettings(), null);
+
+            //Трансформація і запис результату
+            xsltTransform.Transform(pathToXmlData, pathToSaveResult);
+
+        }
+    }
+}
---
+++ WindowsFormsXSLT/Template.xslt
@@ -0,0 +1,44 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+	
+    <xsl:output method="html" indent="yes"/>
+
+    <xsl:template match="/">
+
+		<table border="1" width="100%">
+
+			<tr>
+				<td>№</td>
+				<td>Назва</td>
+				<td>Ціна</td>
+				<td>Валюта</td>
+				<td>Виробник</td>
+			</tr>
+			
+			<xsl:for-each select="root/item">
+
+				<tr>
+					<td>
+						<xsl:value-of select="@index"/>
+					</td>
+					<td>
+						<xsl:value-of select="name"/>
+					</td>
+					<td>
+						<xsl:value-of select="price"/>
+					</td>
+					<td>
+						<xsl:value-of select="currency"/>
+					</td>
+					<td>
+						<xsl:value-of select="manufacturer"/>
+					</td>
+				</tr>
+				
+			</xsl:for-each>
+			
+		</table>	
+		
+    </xsl:template>
+	
+</xsl:stylesheet>
